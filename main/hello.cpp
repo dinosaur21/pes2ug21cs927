8ff511798fbbba786ba3d18f7f96ac1ea660f56c
dinosaur21 <140154294+dinosaur21@users.noreply.github.com>
2024-03-09 13:32:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkin\n";
   cout << "I have successfully built and run\n";
-  cout << "webhook success\n";
+  cout << "webhook successs\n";
   return 0;
 }
